8e57db1d413dc1cb3fe1525faabeed31edb19ca1
dagong <xin.yang@caldron.tech>
2023-12-14 16:42:25 +0800

    [fix]bugFix

--- start.py
+++ start.py
@@ -1,52 +1,27 @@
 import subprocess
-from scrapy import cmdline
-from scrapy.utils.project import get_project_settings
+from crawler_newsday_server.settings import SPIDERS
 from apscheduler.schedulers.blocking import BlockingScheduler
-from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR
-from twisted.internet import reactor, threads
-
-
-def run_spider_in_thread(spider_name):
-    # subprocess.Popen(f"scrapy crawl {spider_name}", shell=True)
-    cmdline.execute(f"scrapy crawl {spider_name}".split())
 
 
 def run_spider(spider_name):
-    threads.blockingCallFromThread(reactor, run_spider_in_thread, spider_name)
-
-
-def create_job():
-    spiders = get_project_settings().get('SPIDERS')
-    sched = BlockingScheduler()
-    for spider_name in spiders:
-        trigger = spiders[spider_name]['trigger']
-        month = spiders[spider_name]['month']
-        day = spiders[spider_name]['day']
-        hour = spiders[spider_name]['hour']
-        minute = spiders[spider_name]['minute']
-        # 添加任务
-        sched.add_job(run_spider, trigger, month=month, day=day, hour=hour, minute=minute, args=[spider_name, ])
-    return sched
-
-
-def spider_listener(spider):
-    if spider.exception:
-        print('任务异常！')
-    else:
-        print('任务正常运行...')
+    print(f'run spider: {spider_name}')
+    subprocess.Popen(f"scrapy crawl {spider_name}", shell=True)
 
 
 def run():
-    sched = create_job()
-    # 配置任务执行完成和执行错误的监听
-    sched.add_listener(spider_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
-    # 设置日志
-    # sched._logger = logging
-    # 开启任务
+    sched = BlockingScheduler()
+    for spider_name in SPIDERS:
+        trigger = SPIDERS[spider_name]['trigger']
+        month = SPIDERS[spider_name]['month']
+        day = SPIDERS[spider_name]['day']
+        hour = SPIDERS[spider_name]['hour']
+        minute = SPIDERS[spider_name]['minute']
+        # 添加任务
+        sched.add_job(run_spider, trigger, month=month, day=day, hour=hour, minute=minute, args=[spider_name, ])
     sched.start()
 
 
 if __name__ == '__main__':
     run()
-    # run_spider('gma_news')
+    # run_spider('rappler')
     # cmdline.execute("scrapy crawl philstar".split())
