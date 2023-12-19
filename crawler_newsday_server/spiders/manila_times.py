c002b7ad831f152b2adb946614c87e38f63f131a
dagong <xin.yang@caldron.tech>
2023-12-19 11:20:16 +0800

    [add]新增manila times新闻采集脚本

--- crawler_newsday_server/spiders/manila_times.py
+++ crawler_newsday_server/spiders/manila_times.py
@@ -9,27 +9,28 @@ from crawler_newsday_server.utils.mongo_utils import MongoDB
 from crawler_newsday_server.utils.date_utils import time_2_timestamp, time_2_isotime, pst_2_strftime, convert_to_beijing_time
 from crawler_newsday_server.items import NewsItem
 import logging
-import time
 
 
-class BusinessWorldSpider(scrapy.Spider):
-    name = "business_world"
-    # allowed_domains = ["bworldonline.com"]
-    # start_urls = ["https://bworldonline.com"]
+class ManilatimesSpider(scrapy.Spider):
+    name = "manila_times"
+    # allowed_domains = ["manilatimes.net"]
+    # start_urls = ["https://manilatimes.net"]
     mongo = MongoDB()
     logger = logging.getLogger(__name__)
     settings = get_project_settings()
-    category_list = ['corporate', 'stock-market', 'banking-finance', 'economy', 'markets', 'the-nation', 'world', 'opinion', 'infographics', 'podcast', 'sparkup', 'spotlight', 'labor-and-management', 'property', 'agribusiness', 'health', 'arts-and-leisure', 'technology', 'velocity', 'special-features', 'sports', 'special-reports', 'bw-launchpad']
+    category_list = ['opinion', 'news', 'regions', 'world', 'business', 'sports', 'entertainment-lifestyle/show-times',
+                     'entertainment-lifestyle/life-times', 'the-sunday-times', 'supplements', 'special-sections',
+                     'public-square', 'legal-advice', 'fast-times', 'campus-press', 'expats-diplomats', 'podcasts']
 
     def start_requests(self):
         for category in self.category_list:
-            for page in range(1, 6):
-                url = f'https://bworldonline.com/{category}/page/{page}/'
+            for page in range(1, 21):
+                url = f'https://www.manilatimes.net/{category}/page/{page}/'
                 yield Request(url, meta={'category': category}, callback=self.parse)
 
     def parse(self, response):
         try:
-            url_list = response.xpath('//div[@class="item-details"]/h3/a/@href').getall()
+            url_list = response.xpath('//div[@class="content-row flex-row"]//div[@class="article-title-h4 ellipsis-2 mr-b10"]/a/@href').getall()
             for url in url_list:
                 _id = md5_string(url)
                 is_repeat = self.mongo.is_repeat(self.settings.get('MONGO_TABLE'), _id)
@@ -52,7 +53,7 @@ class BusinessWorldSpider(scrapy.Spider):
             try:
                 content = ''
                 html = etree.HTML(response.text)
-                content_html_list = html.xpath('//div[@class="td-post-content td-pb-padding-side"]/p')
+                content_html_list = html.xpath('//div[@class="article-body-content"]/p')
                 for content_html in content_html_list:
                     temp_content = ''.join(content_html.xpath('.//text()'))
                     content = content + temp_content + '\n\n'
@@ -98,7 +99,7 @@ class BusinessWorldSpider(scrapy.Spider):
                 item['news_publish_time'] = publish_time
                 item['news_publish_timestamp'] = publish_timestamp
                 item['news_head_pic'] = head_pic
-                item['news_source'] = 'business_world'
+                item['news_source'] = 'manila_times'
                 item['news_category'] = response.meta['category']
                 item['news_language'] = 'en'
                 item['news_country'] = 'ph'
@@ -112,4 +113,4 @@ class BusinessWorldSpider(scrapy.Spider):
                 # print(dict(item))
                 yield item
         except Exception as e:
-            self.logger.error(f'get news content failed, url: {response.url}, error: {e}')
+            self.logger.error(f'parse detail page failed, error: {e}')
